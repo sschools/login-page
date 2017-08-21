e45d34d88cad696bb9196eb8b254ddd4a8ed2ebf
Stephen Schools <stephen.schools>
2017-08-21 08:40:43 -0400

    everything appears functional

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set("views", __dirname + "/views");
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.use(session({
   secret: "elvis",
